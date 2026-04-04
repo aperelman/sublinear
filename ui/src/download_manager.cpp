e20b4ae378bb2e9867d0d021b8ff7bd1eb94521d
Amit Perelman <aperelman@pm.me>
2026-04-04 23:03:54 +0300

    Fixed download completion bug, Button enabling after downloads is done
    Signed-off-by: Amit Perelman <aperelman@pm.me>

--- ui/src/download_manager.cpp
+++ ui/src/download_manager.cpp
@@ -8,22 +8,50 @@ DownloadManager::DownloadManager(QObject *parent) : QObject(parent) {
     manager = new QNetworkAccessManager(this);
 }
 
+void DownloadManager::validateUrl(const QUrl &url) {
+    QNetworkRequest request(url);
+    // HEAD request: Asks the server "Is it there?" without downloading the bytes.
+    QNetworkReply *reply = manager->head(request);
+
+    connect(reply, &QNetworkReply::finished, this, [this, reply, url]() {
+        bool isValid = false;
+        if (reply->error() == QNetworkReply::NoError) {
+            int statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+            if (statusCode >= 200 && statusCode < 400) {
+                isValid = true;
+            }
+        }
+        Q_EMIT urlValidated(url, isValid);
+        reply->deleteLater();
+    });
+}
+
 void DownloadManager::startDownload(const QUrl &url, const QString &savePath) {
     QNetworkRequest request(url);
     QNetworkReply *reply = manager->get(request);
     reply->setProperty("savePath", savePath);
     reply->setProperty("isCatalog", false);
+    m_activeReply = reply;
 
-    // Modern syntax compatible with AUTOMOC
+    connect(reply, &QNetworkReply::downloadProgress, this,
+        [this](qint64 bytesReceived, qint64 bytesTotal) {
+            Q_EMIT progress(bytesReceived, bytesTotal);
+        });
+
     connect(reply, &QNetworkReply::finished, this, [this, reply]() { this->onFinished(reply); });
+}
+
+void DownloadManager::cancelDownload() {
+    if (m_activeReply) {
+        m_activeReply->abort();
+    }
 }
 
 void DownloadManager::startCatalogDownload(const QUrl &url, const QString &savePath) {
     QNetworkRequest request(url);
     QNetworkReply *reply = manager->get(request);
     reply->setProperty("savePath", savePath);
-    reply->setProperty("isCatalog", true); // Flag to identify index updates
-
+    reply->setProperty("isCatalog", true);
     connect(reply, &QNetworkReply::finished, this, [this, reply]() { this->onFinished(reply); });
 }
 
@@ -36,18 +64,19 @@ void DownloadManager::onFinished(QNetworkReply *reply) {
         if (file.open(QIODevice::WriteOnly)) {
             file.write(reply->readAll());
             file.close();
-
-            if (isCatalog) {
-                Q_EMIT catalogDownloaded(true); // Signal successful write for parser
-            }
+            if (isCatalog) Q_EMIT catalogDownloaded(true);
             Q_EMIT finished(savePath);
         } else {
             if (isCatalog) Q_EMIT catalogDownloaded(false);
             Q_EMIT error("Could not open file for writing: " + savePath);
         }
     } else {
-        if (isCatalog) Q_EMIT catalogDownloaded(false);
-        Q_EMIT error("Download failed: " + reply->errorString());
+        // Don't emit error or finished on user-initiated abort
+        if (reply->error() != QNetworkReply::OperationCanceledError) {
+            if (isCatalog) Q_EMIT catalogDownloaded(false);
+            Q_EMIT error("Download failed: " + reply->errorString());
+        }
     }
+    if (reply == m_activeReply) m_activeReply = nullptr;
     reply->deleteLater();
 }
